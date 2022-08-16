603c4704ac9515a315a1efd380e5205dee2257c9
Ricardo bento Caetano <101500710+Ricardokae@users.noreply.github.com>
2022-08-16 03:23:43 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
+/*===== MENU =====*/
 
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    toggleLink = document.querySelector('#nav-toggle i')
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    //active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //remove menu mobile
+    const navMenu =document.querySelector('#nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
