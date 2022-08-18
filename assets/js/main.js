c727d46a187728822a77a883d8f221807861fbcb
Ricardo bento Caetano <101500710+Ricardokae@users.noreply.github.com>
2022-08-18 06:15:34 -0300

    Update main.js
    projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,16 +17,44 @@ showMenu('nav-toggle', 'nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
     //active link
     navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
+    this.classList.add('active');
 
     //remove menu mobile
     const navMenu =document.querySelector('#nav-menu')
-    navMenu.classList.remove('show')
+    navMenu.classList.remove('show');
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//portfolio gallery
+
+let filterContainer = document.querySelector("#gallery-filter");
+let galleryItems = document.querySelectorAll("#gallery-item");
+
+
+filterContainer.addEventListener("click", e =>{
+    if(e.target.classList.contains("filter-item")){
+        //deactivates existing active filter item
+        filterContainer.querySelector(".active").classList.remove("active");
+        //activates new filter item
+        e.target.classList.add("active");
+
+
+        let filterValue = e.target.getAttribute("data-filter");
+        galleryItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
